91a3936377495d0effaecf70ee45a7e0dc863c24
josef <josefsle97@gmail.com>
2022-04-01 15:04:14 +0200

    add formula for Chart

--- src/formula.ts
+++ src/formula.ts
@@ -1,7 +1,18 @@
 import { result, start, growthrate, days } from "./dom-utils";
 
 export function calculateRate() {
+    const daysValue = +days.value;
+    const startValue = Number(start.value);
+    const growthrateValue = +growthrate.value;
+
     result.innerHTML = Math.floor(
-      Number(start.value) * Math.pow(+growthrate.value, +days.value)
+      Number(startValue) * Math.pow(growthrateValue, daysValue)
     ).toString();
+
+    for(let i=1;i<=daysValue;i++){
+        const res = Math.floor(
+            startValue * Math.pow(growthrateValue,i)
+            )
+    }
+
 }

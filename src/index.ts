3a59f747f66c4fd535533cef3c567eb918ecc31d
paulhnn26 <pau.henn.21@lehre.mosbach.dhbw.de>
2022-04-11 09:05:22 +0200

    Suche nach Datum und Name

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,69 @@
-const Datum = document.querySelector('#myInput') as HTMLInputElement;
+const Datum = document.querySelector('#myInput') as HTMLInputElement ;
 const ausgabe = document.querySelector('#Ausgabe') as HTMLLIElement;
+const searchInput = document.querySelector('#searchInput') as HTMLInputElement ;
+
 const element = document.createElement('p')
 const unl = document.querySelector("#parent") as HTMLUListElement;
+const btn = document.querySelector('#button') as HTMLButtonElement;
+const frontend : string = "Frontend";
+btn.addEventListener("click", DateSearch);
+btn.addEventListener("click", DataArray);
+
+let Daten : any[]=  [] ;
 let i;
 
 fetch("https://api.vorlesungsplan.lars-rickert.de/lectures/MOS-WON21")
     .then(res=>res.json())
     .then(data=>{
-        console.log(data[0].name)
         
-        
-        for ( let i = 1; i< data.length; i++ ){
+        Daten = data;
+        console.log(Daten[0].name)
+        // for ( let i = 1; i< Daten.length; i++ ){
+        //   console.log("test")
             
-            // const test = unl.appendChild(document.createElement('li'))
-            // test.textContent = data[i].name + data[i].start;
-            if (data[i].name === "Frontend-Entwicklung (Modul T2)"){
-                const test = unl.appendChild(document.createElement('li'))
-                test.textContent = data[i].name + data[i].start;
-
-            }
+        //   if (Daten[i].name === "Frontend-Entwicklung (Modul T2)"){
+        //       const test = unl.appendChild(document.createElement('li'))
+        //       test.textContent = Daten[i].name + Daten[i].start;
+        //       console.log(data[i].name)
+  
+        //   }}
+        
+        
+       
+        
 
             // element.appendChild(ausgabe)
             // document.body.appendChild(element)
         }
-    })
+    );
+
+   
+    
+    function DataArray (){
+      for ( let i = 1; i< Daten.length; i++ ){
+        console.log(searchInput.value)
+        if (Daten[i].name.includes(searchInput.value)) {
+          console.log("juhu")
+            const test = unl.appendChild(document.createElement('li'))
+            test.textContent = Daten[i].name + Daten[i].start;
+
+        }else{
+          console.log("nein")
+        }
+    }
+  }
+    function DateSearch(){
+      for ( let i = 1; i< Daten.length; i++ ){
+        console.log("test")
+            
+        if (Daten[i].start.includes(Datum.value)){
+          console.log("juhu")
+            const test = unl.appendChild(document.createElement('li'))
+            test.textContent = Daten[i].name + Daten[i].start;
+            
+
+        }
+    }
+    };
+
+    

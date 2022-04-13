20825b3ec34cf90eeb9562e048065258a96a35b9
paulhnn26 <pau.henn.21@lehre.mosbach.dhbw.de>
2022-04-13 17:04:03 +0200

    Wochenanzeige des Studenplans in einem table Ele

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,70 @@
 import {
+  
   isEmptyBindingElement,
   isEmptyStatement,
   textSpanContainsPosition,
 } from "../node_modules/typescript/lib/typescript";
+// import dateFormat, {masks} from "dateformat";
+// dateFormat
 
 const Datum = document.querySelector("#myInput") as HTMLInputElement;
 const ausgabe = document.querySelector("#Ausgabe") as HTMLLIElement;
 const searchInput = document.querySelector("#searchInput") as HTMLInputElement;
+const tableRow = document.querySelector(".tableRow") as HTMLTableRowElement;
+const monday =  document.querySelector("#Monday") as HTMLTableCellElement;
+const tuesday =  document.querySelector("#Tuesday") as HTMLTableCellElement;
+const wednesday = document.querySelector("#Wednesday") as HTMLTableCellElement;
+const thursday =  document.querySelector("#Thursday") as HTMLTableCellElement;
+const friday =  document.querySelector("#Friday") as HTMLTableCellElement;
 
 const unl = document.querySelector("#parent") as HTMLUListElement;
 const btn = document.querySelector("#button") as HTMLButtonElement;
-
+const options :object = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' , hour: 'numeric', minute: 'numeric' };
+const options2 : object = {};
+const options3 : object = {   hour: 'numeric', minute: 'numeric'};
 btn.addEventListener("click", searchLogic);
 
-// btn.addEventListener("click", DataArray);
+
 
 let Daten: any[] = [];
-let i;
 
-fetch("https://api.vorlesungsplan.lars-rickert.de/lectures/MOS-WON21")
+
+fetch("https://api.stuv.app/rapla/lectures/MOS-WON21?archived=true")
   .then((res) => res.json())
   .then((data) => {
     Daten = data;
+    console.log(Daten[0].name)
   });
 
 function DataArray() {
-  for (let i = 1; i < Daten.length; i++) {
-    if (Daten[i].name.includes(searchInput.value)) {      
-      const test = unl.appendChild(document.createElement("li"));
-      test.textContent = Daten[i].name + " Startzeit: " + Daten[i].start;
-      test.classList.add("outputList")
+  console.log("test")
+  for (let i = 0; i < Daten.length; i++) {
+    if (Daten[i].name.includes(searchInput.value)) {     
+      const startTime = new Date(Daten[i].startTime) 
+      const listOutput = unl.appendChild(document.createElement("li"));
+      listOutput.textContent = Daten[i].name + " Startzeit: " + startTime.toLocaleDateString('de-DE', options);
+      
+      listOutput.classList.add("outputList")
     } 
   }
 }
 function DateSearch() {
   for (let i = 1; i < Daten.length; i++) {
-    if (Daten[i].start.includes(Datum.value)) {
-      const test = unl.appendChild(document.createElement("li"));
-      test.textContent = Daten[i].name + " Startzeit: " + Daten[i].start;
-      test.classList.add("outputList")
-      // test.remove();
+    if (Daten[i].startTime.includes(Datum.value)) {
+     
+      const startTime = new Date(Daten[i].startTime)
+      console.log(typeof startTime)
+      
+      const listOutput = unl.appendChild(document.createElement("li"));
+      listOutput.textContent = Daten[i].name + " Startzeit: " + startTime.toLocaleDateString('de-DE', options);
+      listOutput.classList.add("outputList")
+      
     }
   }
 }
 
 function searchLogic() {
-  deleteOutput();
+   deleteOutput();
   if (Datum.value === "" && searchInput.value === "") {
     console.log("keine eingabe");
     alert("Bitte tätigen sie eine eingabe");
@@ -60,3 +79,79 @@ function searchLogic() {
 function deleteOutput(){
   document.querySelectorAll(".outputList").forEach(e => e.remove())
 }
+
+function weaklyTableOutput(){
+  fetch("https://api.stuv.app/rapla/lectures/MOS-WON21?archived=true")
+  .then((res) => res.json())
+  .then((data) => {
+    Daten = data;
+    const dateToday = Date.now();
+    const dateFormatToday = new Date(dateToday)
+    const currentDate = dateFormatToday.toLocaleDateString('de-DE', options2);
+    
+
+    
+    console.log(dateFormatToday)
+  for(let i = 0; i < data.length; i++){
+    const dateCourse = new Date(Daten[i].startTime)
+    const timeToday = dateCourse.toLocaleDateString('de-DE', options3)
+    
+    let currentCourse = dateCourse.toLocaleDateString('de-DE', options2)
+    const curentCourseStart =  dateCourse.toLocaleDateString('de-DE', options3)
+    
+    if(currentDate === currentCourse){
+      // const tableOutput = tableRow.appendChild(document.createElement("td"));
+      // tableOutput.textContent = currentDate + data[i].name;
+      dateFormatToday.getDay();
+      switch (dateFormatToday.getDay()) {
+        case 1: 
+        monday.textContent = curentCourseStart + data[i].name
+        tuesday.textContent = data[i +1].name;
+        wednesday.textContent = data[i +2].name;
+        thursday.textContent = data[i +3].name;
+        friday.textContent = data[i +4].name;
+        break;
+        case 2:
+        tuesday.textContent = curentCourseStart + data[i].name
+        monday.textContent = data[i -1].name;
+        wednesday.textContent = data[i +1].name;
+        thursday.textContent = data[i +2].name;
+        friday.textContent = data[i +3].name;
+        break;
+        
+        case 3: 
+        wednesday.textContent = curentCourseStart + "\n" + data[i].name
+        const d = i;
+        monday.textContent = data[d -2].name;
+        tuesday.textContent = data[d -1].name;
+        thursday.textContent = data[d +1].name;
+        friday.textContent = data[d +2].name;
+        break;
+
+        case 4:
+        thursday.textContent = curentCourseStart + data[i].name
+        monday.textContent = data[i -3].name;
+        tuesday.textContent = data[i -2].name;
+        wednesday.textContent = data[i -1].name;
+        friday.textContent = data[i +1].name;
+        break;
+        case 5:
+        friday.textContent = curentCourseStart + data[i].name
+        monday.textContent = data[i -4].name;
+        tuesday.textContent = data[i -3].name;
+        wednesday.textContent = data[i -2].name;
+        thursday.textContent = data[i -1].name;
+        break;
+
+          
+
+      }
+
+
+    }
+    
+  };
+  });
+  
+};
+weaklyTableOutput();
